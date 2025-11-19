b9886400f191f372f7bbe88d97e581f82fc5a5ec
Filip Tichý <filiptichy888@gmail.com>
2025-11-19 11:42:59 +0100

    Refactor Admin and Employee classes to enhance user management functionality

--- src/Domain/User/Admin.cs
+++ src/Domain/User/Admin.cs
@@ -1,6 +1,32 @@
-public class Admin : User
+public class Admin : Employee
 {
     public Admin(string name, int age, int ssn) : base(name, age, ssn)
     {
+    }
+
+    public void ViewUserInfo(List<User> userList)
+    {
+        foreach (var user in userList)
+        {
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Age);
+            Console.WriteLine(user.SocialSecurityNumber);
+        }
+    }
+    public void CreateUser(List<User> userList, User user)
+    {
+        userList.Add(user);
+    }
+    public void DeleteUser(List<User> userList, User user)
+    {
+        userList.Remove(user);
+    }
+    public void UpdateUser(List<User> userList, User oldUser, User newUser)
+    {
+        int index = userList.IndexOf(oldUser);
+        if (index != -1)
+        {
+            userList[index] = newUser;
+        }
     }
 }
