981ceddcc65eaebc2efd3d20897902022cb60938
Marija Savkina <savk.marija@gmail.com>
2025-11-19 11:28:11 +0100

    Added method ListItems

--- oop-workshop/src/Domain/User/Borrower.cs
+++ oop-workshop/src/Domain/User/Borrower.cs
@@ -3,4 +3,17 @@ public class Borrower : User
     public Borrower(string name, int age, int ssn) : base(name, age, ssn)
     {
     }
+
+    public void ListItems(Media[] items, string type) {
+        foreach (Media item in items)
+        {
+            if (item.GetType().Name == type)
+            {
+                item.DisplayInfo();
+            }
+        }
+
+    }
+
+    public void ViewDetails
 }
