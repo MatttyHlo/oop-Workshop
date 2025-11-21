1f55eeecfbff145e715beb5ce20a057691b9a4b8
Filip Tichý <filiptichy888@gmail.com>
2025-11-21 10:55:26 +0100

    updated program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,14 +1,16 @@
-﻿//implement asking for role
+﻿using oop_workshop.src.Domain.Media;
+
+//implement asking for role
 class Program
 {
     static void Main(string[] args)
     {
         List<Media> collection = new List<Media>();
         Employee employee1 = new Employee("Harry", 69, 0202020202);
-        employee1.AddMedia(collection ,new Movie("Harry Potter", "Some Guy", [action, comedy], 2001, "English", 120));
+        employee1.AddMedia(collection ,new Movie("Harry Potter", "Some Guy", new string[] { "action", "comedy" }, 2001, "English", 120));
         Borrower person1 = new Borrower("Jack", 13, 0101010101);
         person1.ListItems(collection, "Movie"); 
-
-
+        Console.WriteLine(employee1);
+    
     }
 }
